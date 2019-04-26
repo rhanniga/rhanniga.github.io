c9b1838c2fedc84b1565d2968c29747927f1e991
rhanniga <ryan.p.hannigan@gmail.com>
2019-04-26 01:49:48 -0500

    added new template

--- home.php
+++
@@ -1,9 +0,0 @@
-
-<!DOCTYPE php>
-<html >
-<h1>Alana is B E A U T I F U L</h1>
-<h1>Alana is also C U T E</h1>
-
-<h2> For those of you who are interested, you opened this page at: <?php echo date('H:i:s'); ?> (pi time) </h2>
-</html>
-
