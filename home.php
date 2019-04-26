ea1693f0d8480c92bd0c4dc64beb85093f2a1e85
rhanniga <ryan.p.hannigan@gmail.com>
2019-04-26 06:15:02 +0100

    initial commit

---
+++ home.php
@@ -0,0 +1,9 @@
+
+<!DOCTYPE php>
+<html >
+<h1>Alana is B E A U T I F U L</h1>
+<h1>Alana is also C U T E</h1>
+
+<h2> For those of you who are interested, you opened this page at: <?php echo date('H:i:s'); ?> (pi time) </h2>
+</html>
+
